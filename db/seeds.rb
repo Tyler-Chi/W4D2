28d21b9d53508a7d6e08fc34b6caa6a1c4c048cb
App Academy Student <student@appacademy.io>
2017-10-24 17:51:59 -0700

    practice with MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.destroy_all
 
 cat1 = Cat.new(birth_date: '2015-01-20',color: 'brown',name: 'mittens',sex: 'M',description: 'the cat')
 cat2 = Cat.new(birth_date: '2015-01-20',color: 'brown',name: 'mittens',sex: 'M',description: 'the cat')
@@ -28,3 +29,31 @@ cat7.save
 cat8.save
 cat9.save
 cat10.save
+
+rental1 = CatRentalRequest.new(cat_id: cat1.id, status: 'good condition', start_date: '2015-01-20', end_date: '2015-01-25')
+rental1.save
+rental2 = CatRentalRequest.new(cat_id: cat2.id, status: 'good condition', start_date: '2015-01-20', end_date: '2015-01-25')
+rental2.save
+
+rental3 = CatRentalRequest.new(cat_id: cat3.id, status: 'good condition', start_date: '2015-01-20', end_date: '2015-01-25')
+rental3.save
+
+rental4 = CatRentalRequest.new(cat_id: cat4.id, status: 'good condition', start_date: '2015-01-20', end_date: '2015-01-25')
+rental4.save
+
+
+
+
+# class CreateCatRentalRequests < ActiveRecord::Migration[5.1]
+#   def change
+#     create_table :cat_rental_requests do |t|
+#       t.integer :cat_id, null: false
+#       t.date :start_date, null: false
+#       t.date :end_date, null: false
+#       t.string :status, null: false
+#
+#       t.timestamps
+#     end
+#     add_index :cat_rental_requests, :cat_id
+#   end
+# end
